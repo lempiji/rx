750e01c8e023b5976aa131868b754c60e17fac83
lempiji <Koichi.Takio@gmail.com>
2016-01-13 00:33:39 +0900

    Replace Scheduler
    Replace std.concurrency.Scheduler to rx.scheduler.Scheduler
    Add rx.util.EventSignal

--- source/rx/util.d
+++ source/rx/util.d
@@ -1,6 +1,8 @@
 module rx.util;
 
 import core.atomic;
+import core.sync.mutex;
+import core.sync.condition;
 
 T exchange(T, U)(ref shared(T) store, U val)
 {
@@ -17,4 +19,38 @@ unittest
     auto temp = exchange(n, 10);
     assert(n == 10);
     assert(temp == 1);
+}
+
+class EventSignal
+{
+public:
+    this()
+    {
+        _mutex = new Mutex;
+        _condition = new Condition(_mutex);
+    }
+
+public:
+    void setSignal()
+    {
+        synchronized (_mutex)
+        {
+            _signal = true;
+            _condition.notify();
+        }
+    }
+
+    void wait()
+    {
+        synchronized (_mutex)
+        {
+            if (_signal) return;
+            _condition.wait();
+        }
+    }
+
+private:
+    Mutex _mutex;
+    Condition _condition;
+    bool _signal;
 }
