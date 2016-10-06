1df772f8b879cc251fd352aa26b71e41e341ace9
lempiji <lempiji@users.noreply.github.com>
2016-10-07 00:25:36 +0900

    Move rx.algorithm.filter to module

--- source/rx/algorithm/iteration.d
+++ source/rx/algorithm/iteration.d
@@ -8,6 +8,8 @@ import rx.observer;
 import rx.observable;
 import rx.util;
 
+import rx.algorithm.filter;
+
 import core.atomic : cas, atomicLoad;
 import std.functional : unaryFun, binaryFun;
 import std.range : put;
@@ -38,108 +40,6 @@ unittest
 
     auto result = buf.data;
     assert(equal(result, ["0", "2", "4", "6", "8"]));
-}
-
-
-//####################
-// Filter
-//####################
-///Implements the higher order filter function. The predicate is passed to std.functional.unaryFun, and can either accept a string, or any callable that can be executed via pred(element).
-template filter(alias pred)
-{
-    auto filter(TObservable)(auto ref TObservable observable)
-    {
-        static struct FilterObservable
-        {
-            alias ElementType = TObservable.ElementType;
-
-        public:
-            this(TObservable observable)
-            {
-                _observable = observable;
-            }
-
-        public:
-            auto subscribe(TObserver)(TObserver observer)
-            {
-                static struct FilterObserver
-                {
-                    mixin SimpleObserverImpl!(TObserver, ElementType);
-
-                public:
-                    this(TObserver observer)
-                    {
-                        _observer = observer;
-                    }
-                    static if (hasCompleted!TObserver || hasFailure!TObserver)
-                    {
-                        this(TObserver observer, Disposable disposable)
-                        {
-                            _observer = observer;
-                            _disposable = disposable;
-                        }
-                    }
-
-                private:
-                    void putImpl(ElementType obj)
-                    {
-                        alias fun = unaryFun!pred;
-                        if (fun(obj)) _observer.put(obj);
-                    }
-                }
-
-                alias ObserverType = FilterObserver;
-                static if (hasCompleted!TObserver || hasFailure!TObserver)
-                {
-                    auto disposable = new SingleAssignmentDisposable;
-                    disposable.setDisposable(disposableObject(doSubscribe(_observable, FilterObserver(observer, disposable))));
-                    return disposable;
-                }
-                else
-                {
-                    return doSubscribe(_observable, FilterObserver(observer));
-                }
-            }
-
-        private:
-            TObservable _observable;
-        }
-
-        return FilterObservable(observable);
-    }
-}
-///
-unittest
-{
-    import rx.subject;
-    import std.array : appender;
-
-    Subject!int sub = new SubjectObject!int;
-    auto filtered = sub.filter!(n => n % 2 == 0);
-    auto buffer = appender!(int[])();
-    auto disposable = filtered.subscribe(buffer);
-    sub.put(0);
-    sub.put(1);
-    sub.put(2);
-    sub.put(3);
-    import std.algorithm : equal;
-    assert(equal(buffer.data, [0, 2][]));
-}
-unittest
-{
-    import rx.subject;
-    import std.array : appender;
-
-    Subject!int sub = new SubjectObject!int;
-    auto filtered = sub.filter!"a % 2 == 0";
-    auto buffer = appender!(int[])();
-    auto disposable = filtered.subscribe(buffer);
-    sub.put(0);
-    sub.put(1);
-    sub.put(2);
-    sub.put(3);
-    import std.algorithm : equal;
-    assert(equal(buffer.data, [0, 2][]));
 }
 
 
--- source/rx/algorithm/package.d
+++ source/rx/algorithm/package.d
@@ -1,5 +1,6 @@
 module rx.algorithm;
 
 public import rx.algorithm.iteration;
+public import rx.algorithm.filter;
 public import rx.algorithm.searching;
 public import rx.algorithm.timer;
