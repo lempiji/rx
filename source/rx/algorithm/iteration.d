68c8070957e91284453cd192e01de8cb93ac08e7
lempiji <lempiji@users.noreply.github.com>
2016-10-10 23:23:25 +0900

    Move rx.algorithm.iteration.tee to module

--- source/rx/algorithm/iteration.d
+++ source/rx/algorithm/iteration.d
@@ -8,9 +8,10 @@ import rx.observer;
 import rx.observable;
 import rx.util;
 
-import rx.algorithm.filter;
-import rx.algorithm.map;
-import rx.algorithm.scan;
+public import rx.algorithm.filter;
+public import rx.algorithm.map;
+public import rx.algorithm.scan;
+public import rx.algorithm.tee;
 
 import core.atomic : cas, atomicLoad;
 import std.functional : unaryFun, binaryFun;
@@ -46,115 +47,6 @@ unittest
 
 
 
-//####################
-// Tee
-//####################
-struct TeeObserver(alias f, TObserver, E)
-{
-    mixin SimpleObserverImpl!(TObserver, E);
-public:
-    this(TObserver observer)
-    {
-        _observer = observer;
-    }
-    static if (hasCompleted!TObserver || hasFailure!TObserver)
-    {
-        this(TObserver observer, Disposable disposable)
-        {
-            _observer = observer;
-            _disposable = disposable;
-        }
-    }
-private:
-    void putImpl(E obj)
-    {
-        unaryFun!f(obj);
-        _observer.put(obj);
-    }
-}
-struct TeeObservable(alias f, TObservable, E)
-{
-    alias ElementType = E;
-public:
-    this(TObservable observable)
-    {
-        _observable = observable;
-    }
-public:
-    auto subscribe(T)(auto ref T observer)
-    {
-        alias ObserverType = TeeObserver!(f, T, E);
-        static if (hasCompleted!T || hasFailure!T)
-        {
-            auto disposable = new SingleAssignmentDisposable;
-            disposable.setDisposable(disposableObject(doSubscribe(_observable, ObserverType(observer, disposable))));
-            return disposable;
-        }
-        else
-        {
-            return doSubscribe(_observable, ObserverType(observer));
-        }
-    }
-private:
-    TObservable _observable;
-}
-template tee(alias f)
-{
-    TeeObservable!(f, TObservable, TObservable.ElementType) tee(TObservable)(auto ref TObservable observable)
-    {
-        return typeof(return)(observable);
-    }
-}
-unittest
-{
-    import rx.subject;
-    auto sub = new SubjectObject!int;
-    import std.array;
-    auto buf1 = appender!(int[]);
-    auto buf2 = appender!(int[]);
-    auto disposable = sub
-        .tee!(i => buf1.put(i))()
-        .map!(i => i * 2)()
-        .subscribe(buf2);
-
-    sub.put(1);
-    sub.put(2);
-    disposable.dispose();
-    sub.put(3);
-
-    import std.algorithm : equal;
-    assert(equal(buf1.data, [1, 2]));
-    assert(equal(buf2.data, [2, 4]));
-}
-unittest
-{
-    import rx.subject;
-    auto sub = new SubjectObject!int;
-
-    int countPut = 0;
-    int countFailure = 0;
-    struct Test
-    {
-        void put(int) { countPut++; }
-        void failure(Exception) { countFailure++; }
-    }
-
-    int foo(int n)
-    {
-        if (n == 0) throw new Exception("");
-        return n * 2;
-    }
-
-    auto d = sub.tee!foo().doSubscribe(Test());
-
-    assert(countPut == 0);
-    sub.put(1);
-    assert(countPut == 1);
-    assert(countFailure == 0);
-    sub.put(0);
-    assert(countPut == 1);
-    assert(countFailure == 1);
-}
 
 //####################
 // Merge
