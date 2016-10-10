209e183d8f22e5085bacb754fcf4cb1dfe4ef5f3
lempiji <lempiji@users.noreply.github.com>
2016-10-10 23:26:57 +0900

    Move rx.algorithm.iteration.merge to module

--- source/rx/algorithm/iteration.d
+++ source/rx/algorithm/iteration.d
@@ -12,6 +12,7 @@ public import rx.algorithm.filter;
 public import rx.algorithm.map;
 public import rx.algorithm.scan;
 public import rx.algorithm.tee;
+public import rx.algorithm.merge;
 
 import core.atomic : cas, atomicLoad;
 import std.functional : unaryFun, binaryFun;
@@ -44,67 +45,6 @@ unittest
     auto result = buf.data;
     assert(equal(result, ["0", "2", "4", "6", "8"]));
 }
-
-
-
-
-//####################
-// Merge
-//####################
-struct MergeObservable(TObservable1, TObservable2)
-{
-    import std.traits;
-    alias ElementType = CommonType!(TObservable1.ElementType, TObservable2.ElementType);
-
-public:
-    this(TObservable1 o1, TObservable2 o2)
-    {
-        _observable1 = o1;
-        _observable2 = o2;
-    }
-
-public:
-    auto subscribe(T)(T observer)
-    {
-        auto d1 = _observable1.doSubscribe(observer);
-        auto d2 = _observable2.doSubscribe(observer);
-        return new CompositeDisposable(disposableObject(d1), disposableObject(d2));
-    }
-
-private:
-    TObservable1 _observable1;
-    TObservable2 _observable2;
-}
-MergeObservable!(T1, T2) merge(T1, T2)(auto ref T1 observable1, auto ref T2 observable2)
-{
-    return typeof(return)(observable1, observable2);
-}
-
-unittest
-{
-    import rx.subject;
-    auto s1 = new SubjectObject!int;
-    auto s2 = new SubjectObject!short;
-
-    auto merged = s1.merge(s2);
-
-    int count = 0;
-    auto d = merged.doSubscribe((int n){ count++; });
-
-    assert(count == 0);
-    s1.put(1);
-    assert(count == 1);
-    s2.put(2);
-    assert(count == 2);
-
-    d.dispose();
-
-    s1.put(10);
-    assert(count == 2);
-    s2.put(100);
-    assert(count == 2);
-}
-
 
 //####################
 // Fold
