2751847e126e7b26f10ea31aa19454f6069bb6af
lempiji <lempiji@users.noreply.github.com>
2017-11-04 18:26:57 +0900

    Update to example using defer

--- examples/rx-fswatch/source/app.d
+++ examples/rx-fswatch/source/app.d
@@ -2,58 +2,44 @@ import core.thread;
 import core.time;
 import fswatch;
 import rx;
+import std.range : put;
 import std.stdio;
 
 void main()
 {
-    auto watcher = new FileWatcher("./data", true);
+    auto watcher = defer!(FileChangeEvent[], (observer) {
+        auto shutdown = false;
+
+        auto thread = new Thread({
+            auto watch = FileWatch("./data", true);
+
+            while (!shutdown)
+            {
+                auto events = watch.getEvents();
+                if (events.length > 0)
+                {
+                    .put(observer, events);
+                }
+
+                Thread.sleep(100.msecs);
+            }
+        });
+        thread.start();
+
+        return new AnonymouseDisposable({ shutdown = true; });
+    });
 
     auto flatten = watcher.map!(events => from(events)).merge();
     auto changes = flatten.groupBy!(event => event.path).map!(o => o.debounce(2.seconds)).merge();
 
-    changes.doSubscribe((FileChangeEvent event) { writeln(event); });
+    // start FileWatch
+    auto disposable = changes.doSubscribe((FileChangeEvent event) {
+        writeln(event);
+    });
 
-    watcher.start();
     scope (exit)
-        watcher.shutdown();
+        disposable.dispose();
 
     writeln("Plaese Enter to exit.");
     readln();
-}
-
-class FileWatcher : SubjectObject!(FileChangeEvent[])
-{
-    this(string path, bool recursive = false, Duration period = 1000.msecs)
-    {
-        _watch = FileWatch(path, recursive);
-        _period = period;
-    }
-
-    void start()
-    {
-        _thread = new Thread({
-            while (!_shutdown)
-            {
-                auto events = _watch.getEvents();
-                if (events.length > 0)
-                {
-                    this.put(events);
-                }
-
-                Thread.sleep(_period);
-            }
-        });
-        _thread.start();
-    }
-
-    void shutdown()
-    {
-        _shutdown = true;
-    }
-
-private:
-    FileWatch _watch;
-    Thread _thread;
-    bool _shutdown;
-    Duration _period;
 }
